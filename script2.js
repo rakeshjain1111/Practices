4a5d6085e5be5e9adca81fe0c0b036b0e30d8cb2
rakeshjain1111 <rakeshkjain222@gmail.com>
2023-12-15 18:27:41 +0530

    added example

---
+++ script2.js
@@ -0,0 +1,14 @@
+function show(){
+    document.getElementById("mydiv").innerHTML="This is JavaScript";
+}
+
+function show2(){
+   let a= document.getElementById("inp").value;
+   alert(a);
+}
+
+
+function change(){
+    var name=document.getElementById("myheading");
+    name.style.color ="red";
+}
