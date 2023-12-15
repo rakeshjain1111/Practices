9b602bfb608431b08d9b80ea5bb758a20d3dec95
rakeshjain1111 <rakeshkjain222@gmail.com>
2023-12-15 17:49:36 +0530

    example

---
+++ script1.js
@@ -0,0 +1,12 @@
+
+        // var a = document.getElementById("formdiv");
+        // console.log(a);
+   
+// var a =document.getElementById("formdiv").innerHTML;
+// console.log(a);
+
+//  var a =document.getElementById("formdiv").innerText;
+//   console.log(a);
+
+// var a =document.links;
+// console.log(a);
